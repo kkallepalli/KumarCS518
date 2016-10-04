9ed26ff7f6e2fd6f1ad46ec23dbb915f88977ce1
kkallepalli <kkallepalli@cs.odu.edu>
2016-10-04 12:51:43 -0400

    create post fix

--- services/CreatePost.php
+++ services/CreatePost.php
@@ -7,7 +7,7 @@ if ($_SERVER ['REQUEST_METHOD'] == "POST") {
 	$title=$_POST["title"];
 	$content=$_POST["content"];
 	$uid=$_POST["uname"];
-	$sql = "INSERT INTO Question(QTitle, QContent, uid, created_date, views) VALUES ('".$title."','".$content."',".$uid.",'".date("Y-m-d h:i:sa ")."',0)";
+	$sql = "INSERT INTO question(qtitle, qcontent, uid, created_date, views) VALUES ('".$title."','".$content."',".$uid.",'".date("Y-m-d h:i:sa ")."',0)";
 	echo $sql;
 	if(mysqli_query($conn,$sql))
 	{
